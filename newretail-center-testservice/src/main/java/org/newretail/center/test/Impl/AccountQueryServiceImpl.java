890948acd197bcf5b38ff22fde8aa003e806b141
YunxiGithub <2576819987@qq.com>
2017-09-12 17:34:47 +0800

    返回admin名称

--- newretail-center-testservice/src/main/java/org/newretail/center/test/Impl/AccountQueryServiceImpl.java
+++ newretail-center-testservice/src/main/java/org/newretail/center/test/Impl/AccountQueryServiceImpl.java
@@ -5,6 +5,6 @@ import org.newretail.center.test.api.IAccountQueryService;
 public class AccountQueryServiceImpl implements IAccountQueryService {
 
 	public String findAccountNameById(Long id) {
-		return null;
+		return "admin";
 	}
 }
