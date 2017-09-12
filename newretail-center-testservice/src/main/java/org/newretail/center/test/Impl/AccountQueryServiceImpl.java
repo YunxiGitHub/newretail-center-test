1389abd0331f05eb2dcb04c0c93210d6246e1084
YunxiGithub <2576819987@qq.com>
2017-09-12 17:57:29 +0800

    Revert "返回admin名称"
    This reverts commit 890948acd197bcf5b38ff22fde8aa003e806b141.
    
    # Conflicts:
    #	newretail-center-testservice/src/main/java/org/newretail/center/test/Impl/AccountQueryServiceImpl.java

--- newretail-center-testservice/src/main/java/org/newretail/center/test/Impl/AccountQueryServiceImpl.java
+++ newretail-center-testservice/src/main/java/org/newretail/center/test/Impl/AccountQueryServiceImpl.java
@@ -5,7 +5,7 @@ import org.newretail.center.test.api.IAccountQueryService;
 public class AccountQueryServiceImpl implements IAccountQueryService {
 
 	public String findAccountNameById(Long id) {
-		System.out.println("æ‰“å°");
+		System.out.println("´òÓ¡");
 		return "admin";
 	}
 }
