96c1b5ec8f3f2d4a7ea0b09824e96918017d4186
YunxiGithub <2576819987@qq.com>
2017-09-12 17:41:40 +0800

    打印

--- newretail-center-testservice/src/main/java/org/newretail/center/test/Impl/AccountQueryServiceImpl.java
+++ newretail-center-testservice/src/main/java/org/newretail/center/test/Impl/AccountQueryServiceImpl.java
@@ -5,6 +5,7 @@ import org.newretail.center.test.api.IAccountQueryService;
 public class AccountQueryServiceImpl implements IAccountQueryService {
 
 	public String findAccountNameById(Long id) {
+		System.out.println("打印");
 		return "admin";
 	}
 }
